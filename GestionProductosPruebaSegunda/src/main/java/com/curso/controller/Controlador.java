93126bf9ea9286aba2e8f41e1322b99b2837a25e
Admin <Admin@10.250.6.2>
2024-12-17 17:36:30 +0100

    Codigo actualizado

--- GestionProductosPruebaSegunda/src/main/java/com/curso/controller/Controlador.java
+++ GestionProductosPruebaSegunda/src/main/java/com/curso/controller/Controlador.java
@@ -43,12 +43,4 @@ public class Controlador extends HttpServlet {
 		despachador.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
